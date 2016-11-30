32c2fd49ef58c35a3712799ce44a89665872021d
王会洲 <wanghuizhou21@163.com>
2016-11-30 10:24:13 +0800

    上传基本工程

---
+++ HZDatePickerView/HZDatePickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HZDatePickerView
+//
+//  Created by 王会洲 on 16/11/30.
+//  Copyright © 2016年 王会洲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
